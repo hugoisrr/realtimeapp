c5c33c0938f1fb4cfe98329a9b842eb7bdf1a0ba
Hugo Israel Ramirez Soto <84rami@gmail.com>
2020-10-20 12:54:42 +0200

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,17 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 
 Route::post('/likes/{reply}', 'LikesController@likeIt');
 Route::delete('/likes/{reply}', 'LikesController@unLikeIt');
+
+// Route::group([
+
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+
+// ], function ($router) {
+
+//     Route::post('login', 'AuthController@login');
+//     Route::post('logout', 'AuthController@logout');
+//     Route::post('refresh', 'AuthController@refresh');
+//     Route::post('me', 'AuthController@me');
+
+// });
