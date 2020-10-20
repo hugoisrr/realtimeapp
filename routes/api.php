751210d6ae4dc3e4c8ad77bd538971eb80a95de3
Hugo Israel Ramirez Soto <84rami@gmail.com>
2020-10-20 11:17:45 +0200

    Like and Dislike system for reply

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/likes/{reply}', 'LikesController@likeIt');
+Route::delete('/likes/{reply}', 'LikesController@unLikeIt');
